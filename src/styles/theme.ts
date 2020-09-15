6a2a3256576642d1d81b0bdf390e8b0b18271173
Stefano Saffran <stefanosaffran@hotmail.com>
2020-09-15 13:45:56 +0200

    :lipstick: feat: add global styles and theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,34 @@
+export default {
+  colors: {
+    primary: '#3B6AFE',
+    secondary: '#FF8700',
+    terciary: '#FF500F',
+    grey: '#8C91A5',
+    greyLowerOpacity: '#abaebf',
+    lightGrey: '#F5F6FA',
+    darkGrey: '#6C757D',
+    progress: '#20C05C',
+    done: '#FD4F48',
+    label: '#fff',
+    background: '#fff',
+    yellow: '#FFED00',
+  },
+  fontSizes: {
+    default: '1.4rem',
+    large: '1.6rem',
+    small: '1.2rem',
+    tiny: '1rem',
+  },
+  transition: {
+    default: '180ms ease-in-out',
+  },
+  radii: {
+    default: '0.8rem',
+    small: '0.4rem',
+    tiny: '0.2rem',
+  },
+  shadows: {
+    default: '0 0.5rem 2rem rgba(0, 0, 0, 0.08)',
+    flat: '0 0.2rem 0.2rem rgba(0, 0, 0, 0.08)',
+  },
+};
