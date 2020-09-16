d24a23895c0a094e8a95b853f63d9bd231b2e12d
Stefano Saffran <stefanosaffran@hotmail.com>
2020-09-16 22:42:26 +0200

    :sparkles: feat: add task modal

---
+++ src/components/Modal/ModalTask/index.tsx
@@ -0,0 +1,61 @@
+import React, { useCallback, useState, FormEvent } from 'react';
+
+import { v4 as uuid } from 'uuid';
+
+import { Form, ConfirmButton } from './styles';
+
+import { useTask } from '~/context/task';
+
+import Modal from '..';
+
+interface IModalProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+}
+
+const ModalTask = ({ isOpen, setIsOpen }: IModalProps) => {
+  const [title, setTitle] = useState('');
+  const { addTask } = useTask();
+
+  const handleSubmit = useCallback(
+    async (event: FormEvent) => {
+      event.preventDefault();
+      const id = uuid();
+
+      if (title === '') return;
+
+      addTask({
+        id,
+        title,
+        done: false,
+        startedAt: null,
+        isRunning: false,
+        time: 0,
+      });
+      setIsOpen();
+    },
+    [setIsOpen, addTask, title],
+  );
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>Informe o titulo da tarefa</legend>
+
+          <input
+            name="title"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+
+          <ConfirmButton type="submit" variant="primary">
+            Criar
+          </ConfirmButton>
+        </fieldset>
+      </Form>
+    </Modal>
+  );
+};
+
+export default ModalTask;
--- src/pages/Dashboard/SideBar/index.tsx
+++ src/pages/Dashboard/SideBar/index.tsx
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 import { FiPlus } from 'react-icons/fi';
 
 import Summary from './Summary';
+
+import Modal from '~/components/Modal/ModalTask';
+
 import { Container, Wrapper, TaskButton } from './styles';
 
 const SideBar: React.FC = () => {
-  return (
-    <Container>
-      <Wrapper>
-        <TaskButton variant="primary">
-          <FiPlus size={18} />
-          Nova tarefa
-        </TaskButton>
+  const [modalOpen, setModalOpen] = useState(false);
 
-        <Summary />
-      </Wrapper>
-    </Container>
+  const toggleModal = useCallback(() => {
+    setModalOpen(!modalOpen);
+  }, [modalOpen]);
+
+  const handleAddTask = useCallback(() => {
+    toggleModal();
+  }, [toggleModal]);
+
+  return (
+    <>
+      <Container>
+        <Wrapper>
+          <TaskButton variant="primary" onClick={handleAddTask}>
+            <FiPlus size={18} />
+            Nova tarefa
+          </TaskButton>
+
+          <Summary />
+        </Wrapper>
+      </Container>
+      <Modal isOpen={modalOpen} setIsOpen={toggleModal} />
+    </>
   );
 };
 
