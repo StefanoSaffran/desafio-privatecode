75cf2431bd72e710c168e4e9e7b542d048d9773d
Stefano Saffran <stefanosaffran@hotmail.com>
2020-09-16 23:16:35 +0200

    :recycle: chore: update sideBar to use real data

--- src/pages/Dashboard/SideBar/Summary/index.tsx
+++ src/pages/Dashboard/SideBar/Summary/index.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 
+import { useTask } from '~/context/task';
+
 import { Container } from './styles';
 
 const Summary: React.FC = () => {
+  const { getTotal, getInProgress, getDone } = useTask();
   return (
     <Container>
       <h3>Resumo</h3>
 
       <div>
         <strong>Total: </strong>
-        <span>10</span>
+        <span>{getTotal()}</span>
       </div>
       <div>
         <strong>Concluidas: </strong>
-        <span>8</span>
+        <span>{getDone()}</span>
       </div>
       <div>
         <strong>Em andamento: </strong>
-        <span>2</span>
+        <span>{getInProgress()}</span>
       </div>
     </Container>
   );
